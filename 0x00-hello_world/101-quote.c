1a8b24ac8ca97e1f067bbc22b9de4fae5dc84b0f
Universal-E <universal.emmanuel1@gmail.com>
2023-06-18 19:05:51 +0100

    Displaying a string without using printf or puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Displays the string without using printf or puts
- * Return: Always 0 (Success)
+ * Return: 1 (Success)
  */
 int main(void)
 {
-	fput(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 
+	sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 	return (1);
 }
