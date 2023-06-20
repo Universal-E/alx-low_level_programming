e19888202a9c1a37064d53f8ce1fb2de7f4502f1
Universal-E <universal.emmanuel1@gmail.com>
2023-06-20 17:10:44 +0100

    number type identifier

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,18 +1,23 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  * main - Identifies is a number is zero or positive or negative
  * Return: 0 if successful
  */
+
 int main(void)
+
 {
 	int n;
+
 	srand(time(0));
-	n = rand() - RAND_MAX/2;
+
+	n = rand() - RAND_MAX / 2;
 	if (n>0)
 	{
-	printf("%d is positive\n",n);
+	printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
