e9f92ea0ca91bc820a84663dd9a419a6c639b135
Universal-E <universal.emmanuel1@gmail.com>
2023-06-27 03:39:01 +0100

    Prints single base 10 digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single base 10 digits from 0
+ * Return: 0 (success)
+ */
+
+int main(void)
+
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+
+	{
+		printf("%d", a);
+	}
+
+	putchar('\n');
+	return (0);
+}
