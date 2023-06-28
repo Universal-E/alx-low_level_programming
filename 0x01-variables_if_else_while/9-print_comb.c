3aafb65611dbeb83088d3f1aaa922594c755377f
Universal-E <universal.emmanuel1@gmail.com>
2023-06-28 07:48:40 +0100

    prints all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints possible combinations of single digit
+ * Return: 0 (Success)
+ */
+
+int main(void)
+
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+		{
+	putchar(',');
+	putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
