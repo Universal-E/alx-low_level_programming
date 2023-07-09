67b28c92a8be3fac71515eeb18f0dc46a69a7148
Universal-E <universal.emmanuel1@gmail.com>
2023-07-09 23:33:36 +0100

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Display lowercase alphabet 
+ * 10 repeated times
+ * Return: void
+ */
+
+{
+	int alphabet = 0;
+	int multiple;
+
+	while (alphabet++ <= 9)
+	{
+		for (multiple = 'a'; multiple <= 'z'; multiple++)
+		      
+	_putchar(multiple);
+		_putchar(10);
+	}
+}
